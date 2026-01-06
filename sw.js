60816511a33cfab6de5f0be702249318bed270e9
pezzaliapp <pezzalialessandro@gmail.com>
2026-01-06 09:40:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'releasepilot-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './ReleasePilot-192.png',
+  './ReleasePilot-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('./')))
+  );
+});
